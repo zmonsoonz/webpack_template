2abc416e0a687d5588fa32acb4decba1621d236e
Danila <mok-danil28@mail.ru>
2024-06-05 03:21:44 +0300

    Added style loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
+
 module.exports = (env) => {
 
     const isDev = env.mode === 'development'; // проверка на дев мод
+    const isProd = !isDev; // проверка на прод мод
 
     return {    
 
@@ -18,11 +21,26 @@ module.exports = (env) => {
             path: path.resolve(__dirname, 'build'), //путь до выходного файла
             clean: true //чистит директорию от лишних файлов
         },
-
+        module: { //меняет файлы для сборки
+            rules: [
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                      // Creates `style` nodes from JS strings
+                      isDev ? "style-loader" : MiniCssExtractPlugin.loader, // в дев сборке используем обычный style loader
+                      // Translates CSS into CommonJS
+                      "css-loader",
+                      // Compiles Sass to CSS
+                      "sass-loader",
+                    ],
+                },
+            ]
+        },
         plugins: [
             new webpack.ProgressPlugin(), //плагин показывает прогресс сборки (не рекомендуется ипользовать)
             new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }),
             //плагин подгружает html в build
+            !isDev && new MiniCssExtractPlugin() //добавляет css файлы в сборку
         ],
 
         devtool: isDev && 'inline-source-map', //соурс мап для отслеживания ошибок
